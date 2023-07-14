90e2e3a1374a791cc7a3e0d43fbd17cb8ca04dd9
jvltrading <jvltrading@gmail.com>
2023-07-14 14:46:44 +0200

    Completed C# Character Counting Console App
    Console App which counts the number of times a character is present in a string given by a user.

---
+++ Character Count/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Character_Count
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter A New String : ");
+            string str = Console.ReadLine().ToLower();
+
+            if (str.Length > 0)
+            {
+                foreach(char key in str)
+                {
+                    int frequency = 0;
+                    foreach(char c in str)
+                        if (c == key)
+                        {
+                            frequency++;
+                        }
+
+                    Console.WriteLine(key + "   " + frequency);
+                    Console.WriteLine("-------------");
+                }
+            } else
+            {
+                Console.WriteLine("Input is too short, Please Try Again");
+            }
+            Main();
+        }
+    }
+}
